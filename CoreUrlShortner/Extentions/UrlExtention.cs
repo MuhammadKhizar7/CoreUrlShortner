4756955d7671cf7d6a76cc83820096d28d69f6a8
MuhammadKhizar7 <muhammadkhizar7@gmail.com>
2021-12-09 00:25:34 +0500

    feature: add post method to get url and check url is valid or not

---
+++ CoreUrlShortner/Extentions/UrlExtention.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CoreUrlShortner.Extentions
+{
+    public static class UrlExtention
+    {
+        public static bool CheckURLValid(this string url) => Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult) 
+            && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps) && Uri.IsWellFormedUriString(url, UriKind.Absolute);
+
+    }
+}
--- CoreUrlShortner/Program.cs
+++ CoreUrlShortner/Program.cs
@@ -1,3 +1,5 @@
+using CoreUrlShortner.Extentions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,28 +18,19 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/weatherforecast", () =>
+app.MapPost("/", (string url) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    if (url.CheckURLValid())
+    {
+        return Results.Ok(true);
+    }
+    return Results.BadRequest("URL is not valid");
+
+});
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+
